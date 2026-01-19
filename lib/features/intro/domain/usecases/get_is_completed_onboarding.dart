2570705b114e566c886c486158d1410a863be9aa
Mirai3103 <huuhoag1412@gmail.com>
2026-01-19 20:01:53 +0700

    refactor(onboarding): refine exception handling and update import path

--- lib/features/intro/domain/usecases/get_is_completed_onboarding.dart
+++ lib/features/intro/domain/usecases/get_is_completed_onboarding.dart
@@ -1,4 +1,4 @@
-import 'package:fpdart/src/either.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:fpt_ojt/core/error/failures.dart';
 import 'package:fpt_ojt/core/usecase/usecase_interface.dart';
 import 'package:fpt_ojt/features/intro/domain/repositories/onboarding_repository.dart';
