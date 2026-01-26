7910d523099af3f153bf967e63c005115e220a15
Mirai3103 <huuhoag1412@gmail.com>
2026-01-26 16:36:20 +0700

    refactor(auth): update refresh token handling and validation logic
    - Change refreshTokens method to return nullable access and refresh tokens.
    - Implement replaceRefreshToken method in TokenStore for better token management.
    - Update AuthRefreshInterceptor to handle null tokens and improve error handling.
    - Adjust validations for password length in ValidationsConstants.
    - Remove unused email validation in login details screen.

--- lib/features/auth/data/datasources/impl/token_store_impl.dart
+++ lib/features/auth/data/datasources/impl/token_store_impl.dart
@@ -28,8 +28,9 @@ class TokenStoreImpl implements TokenStore {
   }) async {
     if (rememberMe) {
       await _secureKVStorage.set(_refreshTokenKey, refreshToken);
-      _memoryTokenStore[_refreshTokenKey] = refreshToken;
     }
+      _memoryTokenStore[_refreshTokenKey] = refreshToken;
+
   }
 
   @override
@@ -43,4 +44,14 @@ class TokenStoreImpl implements TokenStore {
     await _secureKVStorage.remove(_refreshTokenKey);
     _memoryTokenStore.remove(_refreshTokenKey);
   }
+  
+  @override
+  Future<void> replaceRefreshToken(String newRefreshToken)async {
+    final isExistAtLocalStorage = await _secureKVStorage.get<String>(_refreshTokenKey) != null;
+    if(isExistAtLocalStorage) {
+      await _secureKVStorage.set(_refreshTokenKey, newRefreshToken);
+    }
+    _memoryTokenStore[_refreshTokenKey] = newRefreshToken;
+  }
+  
 }
