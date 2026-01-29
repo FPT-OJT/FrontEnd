35ff23653dcc677ae5463b73e6739c41f421dd51
Mirai3103 <huuhoag1412@gmail.com>
2026-01-29 13:55:43 +0700

    refactor: externalize hardcoded UI values in SearchInput to constants and theme utilities.

--- lib/features/home/presentation/widgets/search_input.dart
+++ lib/features/home/presentation/widgets/search_input.dart
@@ -1,41 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:fpt_ojt/core/theme/app_colors.dart';
+import 'package:fpt_ojt/core/theme/app_text_styles.dart';
+import 'package:fpt_ojt/core/theme/rounded.dart';
+import 'package:fpt_ojt/core/theme/ui_gaps.dart';
 import 'package:fpt_ojt/features/home/presentation/constants/text.dart';
 
 class SearchInput extends StatelessWidget {
   const SearchInput({super.key, this.onTap});
   final void Function()? onTap;
 
+  static const double inputWidth = 335;
+  static const double inputHeight = 48;
+  static Color shadowColor = Colors.black26;
+  static const double shadowBlurRadius = 4;
+  static Offset shadowOffset = const Offset(0, 2);
+  static const double searchIconSize = 24;
+
   @override
   Widget build(BuildContext context) => GestureDetector(
     onTap: onTap,
     child: Container(
-      width: 335,
-      height: 48,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      width: inputWidth,
+      height: inputHeight,
+      padding: const EdgeInsets.symmetric(horizontal: UIGaps.size16),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(5),
+        borderRadius: Rounded.xs,
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withAlpha(25),
-            offset: const Offset(0, 2),
-            blurRadius: 4,
+            color: shadowColor,
+            offset: shadowOffset,
+            blurRadius: shadowBlurRadius,
           ),
         ],
       ),
-      child: const Row(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
             HomeText.searchPlaceholder,
-            style: TextStyle(
+            style: AppTextStyles.bodyExtraSmall.copyWith(
               color: AppColors.primaryForest,
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
             ),
           ),
-          Icon(Icons.search_outlined, size: 24, color: Colors.black54),
+          const Icon(
+            Icons.search_outlined,
+            size: searchIconSize,
+            color: AppColors.primaryForest,
+          ),
         ],
       ),
     ),
